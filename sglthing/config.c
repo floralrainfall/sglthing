d85d6cc3d77da868b6a3bbf377d7f55c16ccac1a
floralrainfall <waternewgatchi@gmail.com>
2023-03-29 09:20:08 -0400

    change config format from sglthing cfg to GKeyFile

--- sglthing/config.c
+++ sglthing/config.c
@@ -3,115 +3,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 void config_load(struct config_file* config, char* file)
 {
-    FILE* config_file = file_open(file, "r");
-    if(config_file)
+    char rscpath[255];
+    file_get_path(&rscpath, 255, file);
+    if(rscpath)
     {
-        printf("sglthing: loading config file %s\n", file);
-        char* file_data = malloc(65535);
-        memset(file_data,0,65535);
-        fread(file_data, 1, 65535, config_file);
-        for(int i = 0; i < 65535; i++)
-            if(file_data[i] == '\n')
-                file_data[i] = ' ';
-        char* token = strtok(file_data, " ");
-        int odd = 1;
-        char val_name[32];
-        while(token != NULL)
+        config->key_file = g_key_file_new();
+        GError* error = NULL; 
+        if(g_key_file_load_from_file(config->key_file, rscpath, 0, &error))
         {
-            if(odd)
-                strncpy(val_name,token, 32);
-            else
-            {
-                config_add_entry(config, val_name, token);
-            }
-            odd = !odd;
-            token = strtok(NULL, " ");
-        }
-        printf("sglthing: loaded %i configuration entries\n", config->config_values);
-        fclose(config_file);
-        free(file_data);
-    }
-    else
-    {
-        printf("sglthing: unable to load config file %s\n", file);
-    }
-}
 
-void config_add(struct config_file* config, char** argv, int argc)
-{
-    int odd = 1;
-    char val_name[32];
-    for(int i = 1; i < argc; i++)
-    {
-        if(odd)
-            strncpy(val_name,argv[i],32);
+        }
         else
         {
-            config_add_entry(config, val_name, argv[i]);
+            printf("sglthing: glib could not open file %s (%s)\n", rscpath, error->message);
         }
-        odd = !odd;
-    }
-}
-
-void config_add_entry(struct config_file* config, char* a, char* b)
-{
-    if(strncmp(a,"load_filesystem",32)==0)
-    {
-        fs_add_directory(b);
     }
     else
     {
-        struct config_value* config_entry = config_value_get(config, a);
-        if(!config_entry)
-            config_entry = &config->config[config->config_values];
-        strncpy(config_entry->name, a, 32);
-        strncpy(config_entry->string_value, b, 64);
-        for(int i = 0; i < 64; i++)
-            if(config_entry->string_value[i] == '\\')
-                config_entry->string_value[i] = ' ';
-        int token_len = strlen(b);
-        config_entry->string_length = token_len;
-        
-        strncpy(config_entry->string_value,b,64);
-        config_entry->type = VT_STRING;
-        int f_r = sscanf(b,"%f",&config_entry->number_value);
-        if(f_r)
-            config_entry->type = VT_NUMBER;
-
-        config->config_values++;
-
-        if(strcmp(config_entry->name,"user_password")!=0)
-            printf("sglthing: arg \t%s\t\t=%s\n",config_entry->name,config_entry->string_value);
+        printf("sglthing: could not find config file %s\n", file);
     }
-}
-
-struct config_value* config_value_get(struct config_file* config, char* name)
-{
-    for(int i = 0; i < config->config_values; i++)
-    {
-        if(strncmp(config->config[i].name,name,32)==0)
-        {
-            return &config->config[i];
-        }
-    }
-    return NULL;
 }
 
 char* config_string_get(struct config_file* config, char* name)
 {
-    struct config_value* v = config_value_get(config, name);
-    if(!v)
-        return "(null)";
-    return &v->string_value[0];
+    return g_key_file_get_string(config->key_file, "sglthing", name, NULL);
 }
 
 float config_number_get(struct config_file* config, char* name)
 {
-    struct config_value* v = config_value_get(config, name);
-    if(!v)
-        return 0.0;
-    return v->number_value;
+    return g_key_file_get_double(config->key_file, "sglthing", name, NULL);
 }
